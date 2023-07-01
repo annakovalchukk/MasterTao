8db67fdcb8ff535b3c8c106984c99de46acc4b4a
Анна Ковальчук <annakovalcuk@MacBook-Air-Anna.local>
2023-07-01 16:37:39 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
     $('.first-screen-slider').slick({
         dots: true,
         infinite: true,
-        appendArrows: '.first-screen-slider__arrows'
-
+        appendArrows: '.first-screen-slider__arrows',
+        responsive: [{
+            breakpoint: 991,
+            settings: {
+              arrows:false,
+            }
+          },
+        ]
     });
 
   });  
