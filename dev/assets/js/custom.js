9211f361bf935dbc15f82add9b33a67e84203cd1
Анна Ковальчук <annakovalcuk@MacBook-Air-Anna.local>
2023-07-12 19:29:49 +0300

    test adv

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -11,7 +11,33 @@ $(document).ready(function() {
             }
         }, ]
     });
+    mobileOnlySlider("#advantages-slider", true, false, 991);
 
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+      var slider = $($slidername);
+      var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        arrows: $arrows,
+        responsive: [
+          {
+            breakpoint: $breakpoint,
+            settings: "unslick"
+          }
+        ]
+      };
+    
+      slider.slick(settings);
+    
+      $(window).on("resize", function () {
+        if ($(window).width() > $breakpoint) {
+          return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+          return slider.slick(settings);
+        }
+      });
+    } // Mobile Only Slider
 });
 
 const checkbox = document.querySelectorAll('[type="checkbox"]')
